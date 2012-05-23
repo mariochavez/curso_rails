f2ed080219f5333d33e26becd644e40431b95ead
Mario A Chavez <mario.chavez@gmail.com>
2012-05-23 18:37:37 -0500

    Commit inicial del proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CursoRails::Application.config.secret_token = 'f0abf18b4af72d2105abc9b48fdbc4d947674e6f2b0dd3ad0c9b59b27dee71d4529b3c3d301899c4cefafb9398dd2c7afc1367be30d4f5c1c18b9091643ec5fa'
